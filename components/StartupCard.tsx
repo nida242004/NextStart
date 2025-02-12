cdbba5b5d4ce580336117e8f9c905f4fbfcc1be9
nida242004 <syedanidafathima1@gmail.com>
2025-02-12 23:18:31 +0530

    search filter working now

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,59 +1,70 @@
-import { formatDate } from '@/lib/utils'
-import { EyeIcon } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { formatDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 import { Button } from "@/components/ui/button";
+import { Author, Startup } from "@/sanity.types";
 
-const StartupCard = ({ post } : { post: StartupTypeCard}) => {
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
-  const { _createdAt, views, author: {authorId, name}, title, category, image, _id, description} = post;
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+  const {
+    _createdAt,
+    views,
+    author,
+    title,
+    category,
+    _id,
+    image,
+    description,
+  } = post;
+
   return (
-    <li className='startup-card group'>
-      <div className='flex-between'>
-        <p className='startup_card_date'>
-          {formatDate(_createdAt)}
-        </p>
-        <div className='flex gap-1.5'>
-          <EyeIcon className='size-6 text-primary'/>
-          <span className='text-16-medium'>{views}</span>
+    <li className="startup-card group">
+      <div className="flex-between">
+        <p className="startup_card_date">{formatDate(_createdAt)}</p>
+        <div className="flex gap-1.5">
+          <EyeIcon className="size-6 text-primary" />
+          <span className="text-16-medium">{views}</span>
         </div>
       </div>
-      <div className='flex-between mt-5 gap-5'>
-        <div className='flex-1'>
-          <Link href={`/user/${authorId}`}>
-            <p className='text-16-medium line-clamp-1'>
-              {name}
-            </p>
+
+      <div className="flex-between mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${author?._id}`}>
+            <p className="text-16-medium line-clamp-1">{author?.name}</p>
           </Link>
           <Link href={`/startup/${_id}`}>
-            <h3 className='text-26-semibold line-clamp-1 mt-2'>
-              {title}
-            </h3>
+            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
           </Link>
         </div>
-        <Link href={`/user/${authorId}`}> 
-          <Image src="https://placehold.co/48x48" alt="placeholder" width={48} height={48} className='rounded-full'/>
+        <Link href={`/user/${author?._id}`}>
+          <Image
+            src={author?.image!}
+            alt={author?.name!}
+            width={48}
+            height={48}
+            className="rounded-full aspect-square object-cover"
+          />
         </Link>
       </div>
+
       <Link href={`/startup/${_id}`}>
-        <p className='startup-card_desc'>
-          {description}
-        </p>
-        <img src={image} alt="placeholder" className='startup-card_img' />
+        <p className="startup-card_desc">{description}</p>
+        <img src={image} alt="placeholder" className="startup-card_img" />
       </Link>
-      <div className='flex-between gap-3 mt-3'>
-        <Link href={`/?query=${category.toLowerCase()}`}>
-          <p className='text-16-medium'>
-            {category}
-          </p>
+
+      <div className="flex-between gap-3 mt-3">
+        <Link href={`/?query=${category?.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
         </Link>
         <Button className="startup-card_btn" asChild>
           <Link href={`/startup/${_id}`}>Details</Link>
         </Button>
       </div>
     </li>
-  )
-}
+  );
+};
 
-export default StartupCard
+export default StartupCard;
