db9b67f8477b3fb39d7eaf9678b11498d01f7695
nida242004 <syedanidafathima1@gmail.com>
2025-02-16 02:18:16 +0530

    err 2

--- components/View.tsx
+++ components/View.tsx
@@ -3,7 +3,7 @@ import Ping from "./Ping";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { client } from "@/sanity/lib/client";
 import { writeClient } from "@/sanity/lib/write-client";
-import { after } from "next/server";
+import { unstable_after as after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   const result = await client
